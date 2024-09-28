2d023ea7c3ae14490fce15bf12f2bc561540794b
Kevin Israel López <65631970+Kevin12-0@users.noreply.github.com>
2024-09-27 20:38:19 -0600

    Variables

---
+++ hello_world/intrucciones.py
@@ -0,0 +1,23 @@
+print(3 + 3)  # suma de 3+3
+
+"""
+suma de varibles en un print
+"""
+
+manzanas = 7
+naranjas = 5
+peras = 8
+print(peras + naranjas + manzanas)
+# Modifica esta línea
+
+""" modificando variables"""
+
+manzanas = 7
+naranjas = 5
+peras = 8
+# print(manzanas + naranjas + peras) mostraría 20
+# Ahora asignamos nuevos valores a las variables
+manzanas = 10
+naranjas = 1
+peras = 1  # Modifica esta línea
+print(manzanas + naranjas + peras)
